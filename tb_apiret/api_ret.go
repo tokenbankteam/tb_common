4169f5aa71cb581bef367f2da91ae7a12ddc3488
changgong1 <1033010459@qq.com>
2019-10-22 12:31:29 +0800

    adding the api Unified return

---
+++ tb_apiret/api_ret.go
@@ -0,0 +1,67 @@
+package tb_apiret
+
+import "github.com/gin-gonic/gin"
+
+const StatusCodeOk int = 200
+const StatusCode204 int = 204
+const ResultOK int = 0
+const ResultParamFailed = 1
+const ResultServiceFailed = 2
+const ResultServerWarn = 3
+
+// CJsonOk the http api handle ok
+func CJsonOk(c *gin.Context, message string) {
+	c.JSON(StatusCodeOk, gin.H{
+		"result":  ResultOK,
+		"message": message,
+	})
+}
+
+// CJsonOkData the http api handle ok, and return data
+func CJsonOkData(c *gin.Context, message string, data interface{}) {
+	c.JSON(StatusCodeOk, gin.H{
+		"result":  ResultOK,
+		"message": message,
+		"data":    data,
+	})
+}
+
+// CJsonParamFailed parse param failed
+func CJsonParamFailed(c *gin.Context, message string) {
+	c.JSON(StatusCodeOk, gin.H{
+		"result":  ResultParamFailed,
+		"message": message,
+	})
+}
+
+// CJsonServerFailed parse param failed
+func CJsonServerFailed(c *gin.Context, message string) {
+	c.JSON(StatusCodeOk, gin.H{
+		"result":  ResultParamFailed,
+		"message": message,
+	})
+}
+
+func CJsonServerWarn(c *gin.Context, message string) {
+	c.JSON(StatusCodeOk, gin.H{
+		"result":  ResultServerWarn,
+		"message": message,
+	})
+}
+
+func CJsonServerWarnData(c *gin.Context, message string, data interface{}) {
+	c.JSON(StatusCodeOk, gin.H{
+		"result":  ResultServerWarn,
+		"message": message,
+		"data":    data,
+	})
+}
+
+// CJson206 parse tb service failed
+func CJson206(c *gin.Context, message string, data interface{}) {
+	c.JSON(StatusCodeOk, gin.H{
+		"result":  ResultServiceFailed,
+		"message": message,
+		"data":    data,
+	})
+}
