41034f0ced51f1e02a79f43f4db85a081ca9e23c
heipacker <heipackermail@gmail.com>
2020-09-14 17:50:24 +0800

    new: 增加设置连接数据库的参数

--- db/config.go
+++ db/config.go
@@ -1,8 +1,32 @@
 package db
 
 type DbInstConfig struct {
-	Driver string
-	Url    string
+	Driver       string
+	Url          string
+	MaxLifetime  int //wait_timeout/2
+	MaxIdleConns int
+	MaxOpenConns int
+}
+
+func (s *DbInstConfig) GetMaxLifetime() int {
+	if s.MaxLifetime <= 0 {
+		return 6
+	}
+	return s.MaxLifetime
+}
+
+func (s *DbInstConfig) GetMaxIdleConns() int {
+	if s.MaxIdleConns <= 0 {
+		return 2
+	}
+	return s.MaxIdleConns
+}
+
+func (s *DbInstConfig) GetMaxOpenConns() int {
+	if s.MaxOpenConns <= 0 {
+		return 0
+	}
+	return s.MaxOpenConns
 }
 
 type DBConfig struct {
--- db/database.go
+++ db/database.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
 	"sync"
+	"time"
 
 	log "github.com/cihub/seelog"
 )
@@ -23,6 +24,10 @@ func NewDatabase(dbName string, config *DBConfig) (*Database, error) {
 		log.Errorf("open database error %v", err)
 		return nil, err
 	}
+
+	database.SetConnMaxLifetime(time.Duration(instConfig.GetMaxLifetime()) * time.Second)
+	database.SetMaxIdleConns(instConfig.GetMaxIdleConns())
+	database.SetMaxOpenConns(instConfig.GetMaxOpenConns())
 
 	if err := database.Ping(); err != nil {
 		log.Errorf("ping database error %v", err)
